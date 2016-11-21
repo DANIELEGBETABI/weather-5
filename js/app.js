234b9c0a679e2b968de185757dd917c7bbf26682
Leslie Heinzen <leslie.heinzen@gmail.com>
2016-11-21 16:42:58 -0500

    major style overhaul, change temperature conversion button text, add geolocation permission denial handling.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-if (navigator.geolocation) {
+if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(function(position) {
   $.getJSON("https://api.wunderground.com/api/099ff2702a458d9e/conditions/q/" + position.coords.latitude + "," + position.coords.longitude + ".json", function(json){
     var icon = json.current_observation.icon_url.replace(/http/g, 'https');
@@ -6,9 +6,11 @@ if (navigator.geolocation) {
     var units = 'F';
     $('#units').on('click', function(){
       if (units === 'F'){
+        this.innerHTML = "Farenheit";
         units = 'C';
         json.current_observation.temp_f = (json.current_observation.temp_f - 32) * (5/9);
       } else {
+        this.innerHTML = "Celsius";
         units = 'F';
         json.current_observation.temp_f = json.current_observation.temp_f * 1.8 + 32;
       }
@@ -23,7 +25,9 @@ if (navigator.geolocation) {
       $('#justTemp').html("<h3>Oops! We're having trouble finding the weather near you.</h3>");
     }
   });
+}, function(error){
+  if (error.code === error.PERMISSION_DENIED){
+    $('#justTemp').html('<h3>Browser geolocation required for this page to report the weather.</h3>');
+  }
 });
-} else {
-  $('#justTemp').html('<h3>Browser geolocation required for this page to report the weather.</h3>');
 }
